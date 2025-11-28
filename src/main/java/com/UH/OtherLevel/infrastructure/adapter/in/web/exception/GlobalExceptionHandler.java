391795f456be242b9a5d05cdf6f37e7b218b0bd8
SebitasDown <areizasebas31@gmail.com>
2025-11-28 13:40:15 -0500

    actu

---
+++ src/main/java/com/UH/OtherLevel/infrastructure/adapter/in/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.UH.OtherLevel.infrastructure.adapter.in.web.exception;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
